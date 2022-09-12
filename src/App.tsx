c7993a23869d6e29ec3a8b7446453fa33bf29405
PBrorson <patrikbrorson@hotmail.com>
2022-09-12 10:25:52 +0200

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p className ="start">
-        Ett erbjudande från Volkswagen AG <a href = "www.volkswagen.se" 
-      className='click'><br></br>Utgivare & juridisk information</a></p>
-      
-      <h3 className="head">Hjälp för appar och <strong>digitala tjänster</strong></h3>
-      <h6 className ="start">Vi svarar på frågor om mobila tjänster, uppkoppling och frågor<br></br> kring laddning och laddbox.</h6>
-      
-      
-      <a href="www.google.se" className='click'>Klicka här</a>
+      <p className="start">
+        Ett erbjudande från Volkswagen AG{" "}
+        <a href="www.volkswagen.se" className="click">
+          <br></br>Utgivare & juridisk information
+        </a>
+      </p>
+
+      <h3 className="head">
+        Hjälp för appar och <strong>digitala tjänster</strong>
+      </h3>
+      <h6 className="start">
+        Vi svarar på frågor om mobila tjänster, uppkoppling och frågor<br></br>{" "}
+        kring laddning och laddbox.
+      </h6>
+
+      <a href="www.google.se" className="click">
+        Klicka här
+      </a>
     </div>
   );
 }
